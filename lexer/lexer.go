58f5932eee9a07746f10b176f6b2b8070042e2da
chadsmith12 <8794805+chadsmith12@users.noreply.github.com>
2025-05-25 15:04:47 -0500

    add initial lexer tests

---
+++ lexer/lexer.go
@@ -0,0 +1,62 @@
+package lexer
+
+import "github.com/chadsmith12/monkey-interpreter/token"
+
+type Lexer struct {
+	input string
+	position int // the current position we are reading
+	readPosition int // will always point to the next position we are reading
+	ch byte // the current character under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLAN, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func newToken(tokenType token.TokenType, literal byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(literal)}
+}
