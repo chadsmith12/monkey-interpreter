2b21b3c5b3fb2875614d769ed4d908c61f13b0d4
chadsmith12 <8794805+chadsmith12@users.noreply.github.com>
2026-06-12 23:51:27 -0500

    start of ast and parser. Started to parse let statements

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import "github.com/chadsmith12/monkey-interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+// in the statement let x = 5; x is the identifier
+// represents an expression as it can sometimes produce a value
+// let y = x; x is an identifier that produces an expression
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
